1255db2fd78f8b29b0a438366e32bcd99fbe4c58
Matt Ferderer <matt@mattferderer.com>
2018-01-30 15:47:27 -0600

    Fix typo in imports

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,10 +1,9 @@
 import * as React from "react"
-import PropTypes from "prop-types"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import config from "../constants"
+import * as config from "../constants"
 
 import "../sass/screen.scss"
 
