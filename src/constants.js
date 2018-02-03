3ae0ae873186212ff2cad0181a45bb3d7214ba6a
Matt Ferderer <contact@mattferderer.com>
2018-02-02 22:50:11 -0600

    Update bio & fix missing image

--- src/constants.js
+++ src/constants.js
@@ -8,6 +8,6 @@ module.exports = {
   LINKEDIN: 'mattferderer',
   GITHUB: 'mattferderer',
   AUTHOR: `Matt Ferderer`,
-  BIO: `I am a Dad, Husband &amp; Developer who just happens to be interested in JavaScript, C#, F#, Elixir, Web Security, Artificial Intelligence, Cryptocurrency and Virtual Reality. Besides family &amp; tech, my interests revolve around fitness, education &amp; business.`,
+  BIO: `I am a Dad, Husband &amp; Developer who just happens to be interested in JavaScript, C#, F#, Elixir, Web Security, Artificial Intelligence and Virtual Reality. Besides family &amp; tech, my interests revolve around fitness, education &amp; business.`,
   AFFILIATE: `<p>I'm constantly learning from a wide assortment of books & websites. The one subscription I never cancel though is Pluralsight. Their library is enormous & they have awesome learning paths that allow you to see how well you know your stuff. Check out my <a href="https://app.pluralsight.com/profile/mattferderer">Pluralsight profile to see what I'm learning &amp; how I rank</a>. If you're interested in giving them a try, they offer a <a href="https://pluralsight.pxf.io/c/1197267/424552/7490">10 day free trial</a>.</p>`
 }
