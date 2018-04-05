bf3521fb138d6a042426e4cdec0a96d0f24b8bf0
Matthew Ferderer <MFerderer@bepc.com>
2018-04-05 16:12:00 -0500

    Add CSP Policy & enforce. Add Expect-CT header.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 const config = require('./src/constants')
+// const csp = require('./csp-policy')
 module.exports = {
   siteMetadata: {
     title: config.SITE_TITLE,
@@ -113,6 +114,54 @@ module.exports = {
                 "'self' ;" +
               "report-uri " +
                 `https://${config.REPORTURI}.report-uri.com/r/d/csp/wizard`,
+
+            `Expect-CT: max-age=0, report-uri=https://${config.REPORTURI}.report-uri.com/r/d/ct/reportOnly`,
+            "Content-Security-Policy: " + 
+              "form-action " +
+                "syndication.twitter.com " +
+                "platform.twitter.com" +
+              "; "+
+              "font-src " +
+                "fonts.gstatic.com " +
+                "'self'" +
+              "; " + 
+              "script-src " +
+                "platform.twitter.com " +
+                "www.google-analytics.com " +
+                "ssl.google-analytics.com	" +
+                "'sha256-sOPJaJAyizbw4qATJmS9gVXZX8gWbcLhF0xS8rWviww=' " +
+                "'sha256-MXY44blwPHWMXg1B2Y7eAqC0crc+p74gapDWjiMqKxA=' " +
+                "'sha256-ml5NCCyvZVRMT9yuDNxgrqBCvXENW2FfNXw6uaG5t5M=' " +
+                "'sha256-Y6ybr3Cm3Rfq7tF/eVtwoGVonvLLyEiO0oDCasqHDV0=' " +
+                "'self'" +
+              "; " +
+              "frame-src " +
+                "syndication.twitter.com " +
+                "platform.twitter.com" +
+              "; " +
+              "img-src " +
+                "stats.g.doubleclick.net " +
+                "www.google-analytics.com " +
+                "ssl.google-analytics.com	" +
+                "'self'" +
+              "; " +
+              "style-src " +
+                "fonts.googleapis.com " +
+                "'sha256-pmQVgiMVejhZ15re6r5Yh22QXGU4AlBwsAVwTDL6aHU=' " +
+                "'sha256-L1sSENuZjDNt2Wq/MK3Z3FDSa4CoFELb7YwM9A2mzxc=' " +
+                "'sha256-zCvYlDs6LsUp0EqrJFjIGUiM/AG2fGlkNrzJ2YiBTG0=' " +
+                "'sha256-BP8N5oc1ukpumaiMwM/GPY3F97l5b3Jw4/yPbrQeKyg=' " +
+                "'self' " +
+              "; " +
+              "worker-src " +
+                "'self'" +
+              "; " +
+              "connect-src " +
+                "'self'; " +
+              "child-src " +
+                "'self' ;" +
+              "report-uri " +
+                `https://${config.REPORTURI}.report-uri.com/r/d/csp/enforce`,                
           ],
         },
       }
