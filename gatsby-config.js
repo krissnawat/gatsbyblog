acd7d8ce162b432ca6dff4ee4fc7aca7ddea1978
Matthew Ferderer <MFerderer@bepc.com>
2018-04-05 15:26:39 -0500

    Add style src csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,6 +99,7 @@ module.exports = {
                 "'sha256-pmQVgiMVejhZ15re6r5Yh22QXGU4AlBwsAVwTDL6aHU=' " +
                 "'sha256-L1sSENuZjDNt2Wq/MK3Z3FDSa4CoFELb7YwM9A2mzxc=' " +
                 "'sha256-zCvYlDs6LsUp0EqrJFjIGUiM/AG2fGlkNrzJ2YiBTG0=' " +
+                "'sha256-BP8N5oc1ukpumaiMwM/GPY3F97l5b3Jw4/yPbrQeKyg=' " +
                 "'self' " +
               "; " +
               "worker-src " +
