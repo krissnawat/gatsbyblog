3712eb896b7c58d10454de0eb781ab3fa7b9e931
Matthew Ferderer <MFerderer@bepc.com>
2018-04-06 09:37:55 -0500

    Move CSP policy to array

---
+++ csp-policy.js
@@ -0,0 +1,56 @@
+// const deepmerge = require('deepmerge');
+
+const cspPolicy = {
+   "form-action": [
+       "syndication.twitter.com",
+    ],
+   "font-src": [
+       "fonts.gstatic.com",
+       "'self'",
+    ],
+   "script-src": [
+       "www.google-analytics.com",
+       "ssl.google-analytics.com",
+    //    "'sha256-sOPJaJAyizbw4qATJmS9gVXZX8gWbcLhF0xS8rWviww='",
+    //    "'sha256-MXY44blwPHWMXg1B2Y7eAqC0crc+p74gapDWjiMqKxA='",
+    //    "'sha256-ml5NCCyvZVRMT9yuDNxgrqBCvXENW2FfNXw6uaG5t5M='",
+    //    "'sha256-Y6ybr3Cm3Rfq7tF/eVtwoGVonvLLyEiO0oDCasqHDV0='",
+       "'self'",
+    ],
+   "img-src": [
+       "stats.g.doubleclick.net",
+       "www.google-analytics.com",
+       "ssl.google-analytics.com",
+       "'self'",
+    ],
+   "style-src": [
+       "fonts.googleapis.com",
+    //    "'sha256-pmQVgiMVejhZ15re6r5Yh22QXGU4AlBwsAVwTDL6aHU='",
+    //    "'sha256-L1sSENuZjDNt2Wq/MK3Z3FDSa4CoFELb7YwM9A2mzxc='",
+    //    "'sha256-zCvYlDs6LsUp0EqrJFjIGUiM/AG2fGlkNrzJ2YiBTG0='",
+    //    "'sha256-BP8N5oc1ukpumaiMwM/GPY3F97l5b3Jw4/yPbrQeKyg='",
+       "'self'",
+    ],
+   "worker-src": [
+       "'self'",
+    ],
+   "connect-src": [
+       "'self'",
+    ],
+   "child-src": [
+       "'self'",
+    ],  
+}
+
+let cspReport = cspPolicy
+
+function reportToString(csp) {
+    Object.keys(csp)
+    .map(type => `${type} ${csp[type].join(" ")};`)
+    .join(" ")
+}
+
+module.exports = {
+    policy: reportToString(cspPolicy),
+    report: reportToString(cspReport)
+}
