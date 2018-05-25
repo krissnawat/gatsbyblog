0db620184b10cbfe92fbdc6da66ba085618cbae1
Matt Ferderer <contact@mattferderer.com>
2018-05-25 18:18:56 -0500

    Add gatsby-remark-autolink-headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -138,6 +138,7 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          `gatsby-remark-autolink-headers`,
           "gatsby-remark-copy-linked-files",
           {
             resolve: `gatsby-remark-prismjs`,
