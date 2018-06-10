1cc4e449b4e1d6505f9cffd396d659def4ddfd1a
Matt Ferderer <contact@mattferderer.com>
2018-06-10 15:08:04 -0500

    Add fav icons

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -38,8 +38,33 @@ const TemplateWrapper = ({ children }) => (
         {
           rel: "stylesheet",
           type: "text/css",
-          href:
-            "//fonts.googleapis.com/css?family=Roboto:400,500,700|Material+Icons",
+          href: "//fonts.googleapis.com/css?family=Roboto:400,500,700|Material+Icons",
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes:"180x180",
+          href:"/apple-touch-icon.png"
+        },
+        {
+          rel: "icon",
+          type:"image/png",
+          sizes:"32x32",
+          href:"/favicon-32x32.png"
+        },
+        {
+          rel: "icon",
+          type:"image/png",
+          sizes:"16x16",
+          href:"/favicon-16x16.png"
+        },
+        {
+          rel: "manifest",
+          href:"/site.webmanifest"
+        },
+        {
+          rel: "mask-icon",
+          href:"/safari-pinned-tab.svg",
+          color:"#5bbad5"
         },
       ]}
       meta={[
@@ -55,7 +80,8 @@ const TemplateWrapper = ({ children }) => (
         { name: "twitter:description", content: config.SITE_DESCRIPTION },
         { name: "twitter:url", content: config.DOMAIN },
         { name: "twitter:site", content: "@" + config.TWITTER },
-
+        { name: "msapplication-TileColor", content: "#00aba9" },
+        { name: "theme-color", content: "#ffffff" },
       ]}
     >
 	<html lang="en" />
